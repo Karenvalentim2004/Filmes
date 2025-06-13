52fd12077aa6dc6a1d7d79771727a018f3949b1f
Karen Valentim <karenvalentim54@gmail.com>
2025-06-12 21:41:16 -0300

    Exibição de filmes da base de dados

--- app.js
+++ app.js
@@ -1,39 +1,19 @@
-// JSON - javascript object notation    
-
-let filme1 = {
-    titulo: "Jurrassic Park 0",
-    foto: "filme1.webp",
-    avaliacao: "10/10",
-    duracao: "1h50m",
+async function fnPegarFilmes(){
+    let filmes = await fetch ("dados-filmes.json")
+    let filmesTratados = await filmes.json()
+    filmesTratados.slice(0, 4).forEach ((filmeAtual)=> {
+        document.querySelector(".lista-filmes").innerHTML += `
+        <div class="card-filme">
+        <img src="${filmeAtual.foto}">
+        <h3>${filmeAtual.titulo}</h3>
+        <span> ⭐ ${filmeAtual.avaliacao}</span>
+        </div>
+         `
+    })
 }
 
-let filme2 = {
-    titulo: "Karate Kid Lendas",
-    foto: "filme2.jpg",
-    avaliacao: "08/10",
-    duracao: "1h30m",
-}
 
-let filme3 = {
-    titulo: "Lilo e Stitch",
-    foto: "filme3.jpg",
-    avaliacao: "10/10",
-    duracao: "2h30m",
-}
-
-let filme4 = {
-    titulo: "Pecadores",
-    foto: "filme4.jpg",
-    avaliacao: "07/10",
-    duracao: "1h40m",
-}
-
-let filme5 = {
-    titulo: "Princesa e o Sapo",
-    foto: "filme5.jpg",
-    avaliacao: "10/10",
-    duracao: "2:00",
-}
+fnPegarFilmes()
 
 function fnMontarCartao(filmeAtual) {
     document.querySelector(".lista-filmes").innerHTML += `
@@ -44,16 +24,3 @@ function fnMontarCartao(filmeAtual) {
         </div>
          `
 }
-
-//DOM - Document Object Model
-let todosOsFilmes = [filme1, filme2, filme3, filme4, filme5]
-
-todosOsFilmes.forEach((filmeAtual) => {
-    document.querySelector(".lista-filmes").innerHTML += `
-        <div class="card-filme">
-        <img src="img/${filmeAtual.foto}">
-        <h3>${filmeAtual.titulo}</h3>
-        <span> ⭐ ${filmeAtual.avaliacao}</span>
-        </div>
-         `
-})
